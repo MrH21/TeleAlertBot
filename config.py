17c82bb90d6fc963d459b4ee5f61ce14773b344f
Hendrik Oosthuizen <hendrik.oosthuizen@live.com>
2025-08-11 12:41:50 +0200

    config update

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 import os
 import logging
-from fastapi import FastAPI, Request
-from telegram import Update
-from telegram.ext import Application
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")

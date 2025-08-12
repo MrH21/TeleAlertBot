d1f3e46841c6bc4626145dc1adaae3e06815925b
Hendrik Oosthuizen <hendrik.oosthuizen@live.com>
2025-08-12 14:06:47 +0200

    Multi alert update

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 import logging
 
-#BOT_TOKEN = os.getenv("BOT_TOKEN")
-BOT_TOKEN = "8436169622:AAFxmzXV3u5MPU5m5ucV36nY2JiTCe9a66I"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+#BOT_TOKEN = "8436169622:AAFxmzXV3u5MPU5m5ucV36nY2JiTCe9a66I"
 WEBHOOK_PATH = "/webhook"
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 

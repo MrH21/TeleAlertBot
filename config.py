bb742d56e18fb49e13641661eca5375558056510
Hendrik Oosthuizen <hendrik.oosthuizen@live.com>
2025-08-29 14:36:19 +0200

    Webhook update

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ import os
 import logging
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_EXT = "/telegram_webhook"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-WEBHOOK_PATH = f"/{WEBHOOK_URL}{WEBHOOK_EXT}"
+WEBHOOK_PATH = "/telegram_webhook" #Telegram webhook
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
 # Logging

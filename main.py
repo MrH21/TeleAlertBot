cafa503c016f72cd263511d670470bec6d6c4b37
Hendrik Oosthuizen <hendrik.oosthuizen@live.com>
2025-08-29 14:56:43 +0200

    Webhook update for telegram

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def lemon_webhook(request: Request):
 # --------------------------
 # Telegram webhook
 # --------------------------
-@app.post(WEBHOOK_PATH)
+@app.post("/telegram_webhook")
 async def telegram_webhook(request: Request):
     try:    
         data = await request.json()
